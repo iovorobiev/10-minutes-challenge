de6115abeaffaba58dd64325d26e66ac0c959fe0
Ilya Vorobiev <ilya.vorobiev@bandlab.com>
2018-10-13 19:47:55 +0800

    Initial commit

---
+++ Assets/Ball.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Ball : MonoBehaviour {
+
+    bool isSticked = true;
+
+    public void updatePosition(Vector2 position) {
+        if (isSticked)
+        {
+            transform.position = new Vector2(position.x, transform.position.y);
+        }
+    }
+
+	// Update is called once per frame
+	void Update () {
+        if (!isSticked) return;
+        if (Input.GetMouseButtonDown(0)) {
+            var body = GetComponent<Rigidbody2D>();
+            body.isKinematic = false;
+            body.velocity = new Vector2(0f, 10f);
+            isSticked = false;
+        }
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
---
+++ Assets/Platform.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platform : MonoBehaviour {
+
+    public Ball ball;
+
+	// Update is called once per frame
+	void Update () {
+        var position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = new Vector2(position.x, transform.position.y);
+        ball.updatePosition(transform.position);
+	}
+}
